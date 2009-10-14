704b8743f6552cd5a2eb3b9e00b5ffebccfd958b
Yura Zenevich <yzenevich@ocad.ca>
2009-10-14 20:36:24 +0000

    FLUID-3192: Updated the copyright for KettleServlet.java
    svn path=/incubator/engage-sketches/; revision=8329

--- trunk/src/java/org/fluidproject/kettle/KettleServlet.java
+++ trunk/src/java/org/fluidproject/kettle/KettleServlet.java
@@ -1,6 +1,39 @@
-/*
- * Created on 19 Jun 2009
- */
+/**
+* Portions of this software are covered under the following license:
+*
+* Copyright (c) 2009 Thomas Robinson <tlrobinson.net>
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy of 
+* this software and associated documentation files (the “Software”), to deal in 
+* the Software without restriction, including without limitation the rights to 
+* use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies 
+* of the Software, and to permit persons to whom the Software is furnished to 
+* do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in all 
+* copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+* AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN 
+* ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION 
+* WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+* 
+* ------
+* 
+* Other portions, contributed by the Fluid community, are covered under the following license:
+* 
+* Copyright 2008-2009 University of Cambridge
+*
+* Licensed under the Educational Community License (ECL), Version 2.0 or the New
+* BSD license. You may not use this file except in compliance with one these
+* Licenses.
+*
+* You may obtain a copy of the ECL 2.0 License and BSD License at
+* https://source.fluidproject.org/svn/LICENSE.txt
+*/
+
 package org.fluidproject.kettle;
 
 import java.util.Map;
@@ -16,9 +49,8 @@ import org.mozilla.javascript.Function;
 
 /**
  * The root handler servlet for the Kettle framework. Taken with thanks and
- * adapted from "JackServlet".
- * 
- * @author Antranig Basman (amb26@ponder.org.uk)
+ * adapted from "JackServlet". 
+ * http://github.com/tlrobinson/jack-servlet/blob/master/src/org/jackjs/JackServlet.java
  * 
  */
 
