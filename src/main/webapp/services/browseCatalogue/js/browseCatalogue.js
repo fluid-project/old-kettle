b24cb0ff355249540fb9abd9f85a8781713cdbc3
Michelle D'Souza <michelled33@gmail.com>
2010-01-19 21:23:15 +0000

    ENGAGE-248: Change the model for the Browse Catalogue datafeed to match the Browse data feed.
    svn path=/fluid/engage/fluid-engage-kettle/trunk/; revision=9043

--- src/main/webapp/services/browseCatalogue/js/browseCatalogue.js
+++ src/main/webapp/services/browseCatalogue/js/browseCatalogue.js
@@ -11,95 +11,88 @@ https://source.fluidproject.org/svn/LICENSE.txt
 
 // Declare dependencies.
 /*global jQuery, fluid*/
+"use strict";
 
 fluid = fluid || {};
 fluid.catalogueService = fluid.catalogueService || {};
 
 (function ($) {
-	
-	var errorCallback = function (XMLHttpRequest, textStatus, errorThrown) {
-	    fluid.log("XMLHttpRequest: " + XMLHttpRequest);
-	    fluid.log("Status: " + textStatus);
-	    fluid.log("Error: " + errorThrown);
-	    return [500, {"Content-Type": "text/plain"}, errorThrown];
-	};
-	
-	var ajaxCall = function (url, success, error) {
-		$.ajax({
+    
+    var errorCallback = function (XMLHttpRequest, textStatus, errorThrown) {
+        fluid.log("XMLHttpRequest: " + XMLHttpRequest);
+        fluid.log("Status: " + textStatus);
+        fluid.log("Error: " + errorThrown);
+        return [500, {"Content-Type": "text/plain"}, errorThrown];
+    };
+    
+    var ajaxCall = function (url, success, error) {
+        $.ajax({
             url: url,
             dataType: "json",
             asyn: false,
             success: success,
             error: error
-	    });
-	};
-	
-	var getAjax = function (url, error) {
-		var data;
-	    var success = function (returnedData, status) {
-	        data = JSON.parse(returnedData.substring(0, returnedData.length - 1)); //BUG - Doesn't parse with last \n.
-	    };	    
-	    ajaxCall(url, success, error);	    
-	    return data;
-	};
-	
-	var compileDatabaseURL = function (params, config) {
-		return fluid.stringTemplate(config.viewURLTemplateWithKey, {
-			dbName: params.db || "", 
-			view: config.views.catalogueArtifacts, 
-			key: JSON.stringify({
-				exhibitTitle: params.exhibition,
-				sectionTitle: params.title
-			})
-		});
-	};
-	
-	var buildList = function (artifacts) {
-		return {
-			listOptions: {
-				links: artifacts
-			}
-		};
-	};
-	
-	var getData = function (errorCallback, params, config) {
-		var url = compileDatabaseURL(params, config);
-		var rawData = getAjax(url, errorCallback);
-		var dbName = params.db + "_catalogueArtifacts";
-		var data = fluid.engage.mapModel(rawData.rows[0], dbName);
-		var model = {
-	        strings: {
-	            title: data.exhibitionTitle + " / " + data.sectionTitle
-	        },
-			useCabinet: false,
-	        lists: [
-                buildList(data.sectionArtifacts)
-	        ]
-	    };		
-		return JSON.stringify(model);
-	};
-	
-	fluid.catalogueService.initBrowseCatalogueDataFeed = function (config, app) {
-	    var browseCatalogueDataHandler = function (env) {
-	        return [200, {"Content-Type": "text/plain"}, getData(errorCallback, env.urlState.params, config)];
-	    };
-	
-	    var acceptor = fluid.engage.makeAcceptorForResource("browse", "json", browseCatalogueDataHandler);
-	    fluid.engage.mountAcceptor(app, "catalogue", acceptor);
-	};
-	
-	fluid.catalogueService.initBrowseCatalogueService = function (config, app) {
-	    var handler = fluid.engage.mountRenderHandler({
-	        config: config,
-	        app: app,
-	        target: "catalogue/",
-	        source: "components/browse/html/",
-	        sourceMountRelative: "engage"
-	    });
-	        
+        });
+    };
+    
+    var getAjax = function (url, error) {
+        var data;
+        var success = function (returnedData, status) {
+            data = JSON.parse(returnedData.substring(0, returnedData.length - 1)); //BUG - Doesn't parse with last \n.
+        };        
+        ajaxCall(url, success, error);        
+        return data;
+    };
+    
+    var compileDatabaseURL = function (params, config) {
+        return fluid.stringTemplate(config.viewURLTemplateWithKey, {
+            dbName: params.db || "", 
+            view: config.views.catalogueArtifacts, 
+            key: JSON.stringify({
+                exhibitTitle: params.exhibition,
+                sectionTitle: params.title
+            })
+        });
+    };
+    
+    var getData = function (errorCallback, params, config) {
+        var url = compileDatabaseURL(params, config);
+        var rawData = getAjax(url, errorCallback);
+        var dbName = params.db + "_catalogueArtifacts";
+        var data = fluid.engage.mapModel(rawData.rows[0], dbName);
+        
+        var model = {
+            categories: [
+                {
+                    name: data.exhibitionTitle + " / " + data.sectionTitle,
+                    artifacts: data.sectionArtifacts
+                }
+            ]
+        };        
+        return JSON.stringify(model);
+    };
+    
+    fluid.catalogueService.initBrowseCatalogueDataFeed = function (config, app) {
+        var browseCatalogueDataHandler = function (env) {
+            return [200, {"Content-Type": "text/plain"}, getData(errorCallback, env.urlState.params, config)];
+        };
+    
+        var acceptor = fluid.engage.makeAcceptorForResource("browse", "json", browseCatalogueDataHandler);
+        fluid.engage.mountAcceptor(app, "catalogue", acceptor);
+    };
+    
+    fluid.catalogueService.initBrowseCatalogueService = function (config, app) {
+        var handler = fluid.engage.mountRenderHandler({
+            config: config,
+            app: app,
+            target: "catalogue/",
+            source: "components/browse/html/",
+            sourceMountRelative: "engage"
+        });
+            
         handler.registerProducer("browse", function (context, env) {
             return {};
         });
-	        
+            
     };    
 })(jQuery);
