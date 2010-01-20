44c37ded7019a009cc94ffa90b5d3d1e206d5eb9
Michelle D'Souza <michelled33@gmail.com>
2010-01-20 20:51:40 +0000

    NOJIRA: linting
    svn path=/fluid/engage/fluid-engage-kettle/trunk/; revision=9071

--- src/main/webapp/services/exhibition/js/exhibition.js
+++ src/main/webapp/services/exhibition/js/exhibition.js
@@ -11,128 +11,129 @@ https://source.fluidproject.org/svn/LICENSE.txt
 
 // Declare dependencies.
 /*global jQuery, fluid*/
+"use strict";
 
 fluid = fluid || {};
 fluid.exhibitionService = fluid.exhibitionService || {};
 
 (function ($) {
-	
-	var errorCallback = function (XMLHttpRequest, textStatus, errorThrown) {
-	    fluid.log("XMLHttpRequest: " + XMLHttpRequest);
-	    fluid.log("Status: " + textStatus);
-	    fluid.log("Error: " + errorThrown);
-	    return [500, {"Content-Type": "text/plain"}, errorThrown];
-	};
-	
-	var ajaxCall = function (url, success, error) {
-		$.ajax({
+    
+    var errorCallback = function (XMLHttpRequest, textStatus, errorThrown) {
+        fluid.log("XMLHttpRequest: " + XMLHttpRequest);
+        fluid.log("Status: " + textStatus);
+        fluid.log("Error: " + errorThrown);
+        return [500, {"Content-Type": "text/plain"}, errorThrown];
+    };
+    
+    var ajaxCall = function (url, success, error) {
+        $.ajax({
             url: url,
             dataType: "json",
             asyn: false,
             success: success,
             error: error
-	    });
-	};
-	
-	var getAjax = function (url, error) {
-		var data;
-	    var success = function (returnedData, status) {
-	        data = JSON.parse(returnedData.substring(0, returnedData.length - 1)); //BUG - Doesn't parse with last \n.
-	    };	    
-	    ajaxCall(url, success, error);	    
-	    return data;
-	};
-	
-	var buildCategory = function (categoryName, categoryList) {
-		return {
-			name: fluid.stringTemplate(categoryName + " (%num)", {num: categoryList.length}),
-			artifacts: categoryList
-		};
-	};
-	
-	var compileDatabaseURL = function (db, config) {
-		return fluid.stringTemplate(config.viewURLTemplate, 
+        });
+    };
+    
+    var getAjax = function (url, error) {
+        var data;
+        var success = function (returnedData, status) {
+            data = JSON.parse(returnedData.substring(0, returnedData.length - 1)); //BUG - Doesn't parse with last \n.
+        };        
+        ajaxCall(url, success, error);        
+        return data;
+    };
+    
+    var buildCategory = function (categoryName, categoryList) {
+        return {
+            name: fluid.stringTemplate(categoryName + " (%num)", {num: categoryList.length}),
+            artifacts: categoryList
+        };
+    };
+    
+    var compileDatabaseURL = function (db, config) {
+        return fluid.stringTemplate(config.viewURLTemplate, 
             {dbName: db + "_exhibitions" || "", view: config.views.exhibitions});
-	};
-	
-	var compileTargetURL = function (URLBase, params) {
-		return URLBase + "?" + $.param(params);
-	};
-	
-	var getData = function (errorCallback, db, config) {
-		var url = compileDatabaseURL(db, config);
-		var rawData = getAjax(url, errorCallback);
-		var dbName = db + "_exhibitions";
-		var baseExhibitionURL = "view.html";
-		var data = fluid.transform(rawData.rows, function (value) {
-			return fluid.engage.mapModel(value, dbName);
-		});
-		
-		var currentExhibitions = $.map(data, function (value) {
-			return value.isCurrent === "yes" ? {
-				url: compileTargetURL(baseExhibitionURL, {
+    };
+    
+    var compileTargetURL = function (URLBase, params) {
+        return URLBase + "?" + $.param(params);
+    };
+    
+    var getData = function (errorCallback, db, config) {
+        var url = compileDatabaseURL(db, config);
+        var rawData = getAjax(url, errorCallback);
+        var dbName = db + "_exhibitions";
+        var baseExhibitionURL = "view.html";
+        var data = fluid.transform(rawData.rows, function (value) {
+            return fluid.engage.mapModel(value, dbName);
+        });
+        
+        var currentExhibitions = $.map(data, function (value) {
+            return value.isCurrent === "yes" ? {
+                url: compileTargetURL(baseExhibitionURL, {
                     db: dbName,
                     title: value.title
                 }),
-				imageUrl: value.image,
-				title: value.title,
-				description: value.displayDate === "Permanent exhibition" ? "Permanent" : "Through " + value.endDate
-			} : null;
-		});
-		var upcomingExhibitions = $.map(data, function (value) {
-			return value.isCurrent === "no" ? {
-				url: compileTargetURL(baseExhibitionURL, {
+                imageUrl: value.image,
+                title: value.title,
+                description: value.displayDate === "Permanent exhibition" ? "Permanent" : "Through " + value.endDate
+            } : null;
+        });
+        var upcomingExhibitions = $.map(data, function (value) {
+            return value.isCurrent === "no" ? {
+                url: compileTargetURL(baseExhibitionURL, {
                     db: dbName,
                     title: value.title
                 }),
-				imageUrl: value.image,
-				title: value.title,
-				description: value.displayDate
-			} : null;
-		});
-		var model = {
-	        categories: [
-	        	buildCategory("Current Exhibitions", currentExhibitions), 
-	        	buildCategory("Upcoming Exhibitions", upcomingExhibitions)
-	        ]
-	    };		
-		return JSON.stringify(model);
-	};
-	
-	fluid.exhibitionService.initExhibitionsDataFeed = function (config, app) {
-	    var exhibitionsDataHandler = function (env) {
-	        return [200, {"Content-Type": "text/plain"}, getData(errorCallback, env.urlState.params.db, config)];
-	    };
-	
-	    var acceptor = fluid.engage.makeAcceptorForResource("browse", "json", exhibitionsDataHandler);
-	    fluid.engage.mountAcceptor(app, "exhibitions", acceptor);
-	};
-		
-	fluid.exhibitionService.initExhibitionsService = function (config, app) {
-	    var handler = fluid.engage.mountRenderHandler({
-	        config: config,
-	        app: app,
-	        target: "exhibitions/",
-	        source: "components/browse/html/",
-	        sourceMountRelative: "engage"
-	    });
-	        
+                imageUrl: value.image,
+                title: value.title,
+                description: value.displayDate
+            } : null;
+        });
+        var model = {
+            categories: [
+                buildCategory("Current Exhibitions", currentExhibitions), 
+                buildCategory("Upcoming Exhibitions", upcomingExhibitions)
+            ]
+        };        
+        return JSON.stringify(model);
+    };
+    
+    fluid.exhibitionService.initExhibitionsDataFeed = function (config, app) {
+        var exhibitionsDataHandler = function (env) {
+            return [200, {"Content-Type": "text/plain"}, getData(errorCallback, env.urlState.params.db, config)];
+        };
+    
+        var acceptor = fluid.engage.makeAcceptorForResource("browse", "json", exhibitionsDataHandler);
+        fluid.engage.mountAcceptor(app, "exhibitions", acceptor);
+    };
+        
+    fluid.exhibitionService.initExhibitionsService = function (config, app) {
+        var handler = fluid.engage.mountRenderHandler({
+            config: config,
+            app: app,
+            target: "exhibitions/",
+            source: "components/browse/html/",
+            sourceMountRelative: "engage"
+        });
+            
         handler.registerProducer("browse", function (context, env) {
             var data = getData(errorCallback, context.urlState.params.db, config);
-        	var options = {
-        			model: JSON.parse(data),
-        			useCabinet: true,
-        			// TODO: This string needs to be internationalized
-        			title: "Exhibitions"
-        	};
+            var options = {
+                model: JSON.parse(data),
+                useCabinet: true,
+                // TODO: This string needs to be internationalized
+                title: "Exhibitions"
+            };
 
-        	var args = [".flc-browse", options];
-        	var initBlock = {ID: "flc-initBlock", functionname: "fluid.browse", 
+            var args = [".flc-browse", options];
+            var initBlock = {ID: "flc-initBlock", functionname: "fluid.browse", 
                 "arguments": args};
-        	
+            
             return initBlock;
         });
-	        
+            
     };
     
 })(jQuery);
