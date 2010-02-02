371a697c2540bf003c0c6409eb6409c937087ae0
Antranig Basman <antranig.basman@colorado.edu>
2010-02-02 19:32:57 +0000

    ENGAGE-246: Added test for correct transport of UTF-8 characters
    svn path=/fluid/engage/fluid-engage-kettle/trunk/; revision=9231

--- src/test/java/org/fluidproject/engage/TestENGAGE246.java
+++ src/test/java/org/fluidproject/engage/TestENGAGE246.java
@@ -45,19 +45,22 @@ public class TestENGAGE246 extends TestCase {
         InputStream confis = getResource("ENGAGE-246-config.json");
         List patterns = (List) provider.readObject(new ArrayList(), confis);
         confis.close();
-//        Map root = readJSON(patterns, 0);
-//
-//        Object thing = root.get("exhibit");
-//        assertNotNull("Required exhibit", thing);
-//
-//        String JSON = provider.toString(root);
-//        assertEquals("Comment elided", -1, JSON.indexOf("XStandard"));
-//        assertEquals("p is not key", -1, JSON.indexOf("\"p\":"));
+
+        Map root = readJSON(patterns, 0);
+
+        Object thing = root.get("exhibit");
+        assertNotNull("Required exhibit", thing);
+
+        String JSON = provider.toString(root);
+        assertEquals("Comment elided", -1, JSON.indexOf("XStandard"));
+        assertEquals("p is not key", -1, JSON.indexOf("\"p\":"));
+        assertTrue("Found Montréal", JSON.indexOf("Montréal") != -1);
         
         Map root2 = readJSON(patterns, 1);
         String JSON2 = provider.toString(root2);
         assertEquals("p is not key", -1, JSON2.indexOf("\"p\":"));
         String introduction = (String) ((Map)root2.get("exhibit")).get("introduction");
         assertTrue("Found <p>", introduction.startsWith("<p>"));
+
     }
 }
